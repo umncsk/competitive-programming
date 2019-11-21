f8d9658925c03d37099a39a1fd8f39053966c93f
umncsk <umnc.sk@gmail.com>
2019-11-21 15:22:08 +0900

    delete py

--- AtCoder/fill_b/abc138_b.py
+++
@@ -1,7 +0,0 @@
-n = int(input())
-a = list(map(int, input().split()))
-
-for i in range(len(a)):
-    a[i] = 1/a[i]
-print(1/sum(a))
-
--- AtCoder/fill_b/abc139_b.py
+++
@@ -1,8 +0,0 @@
-a,b = map(int, input().split())
-
-ans = 0
-cnt = 1
-while cnt < b:
-    cnt += a - 1
-    ans += 1
-print(ans)
